6f87a957f009e3a9b820486564a5653354a7fa99
Jiahao Si <jiahao.si@shopee.com>
2022-09-26 17:54:04 +0800

    random return

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -17,6 +19,30 @@ func Hello(name string) (string, error) {
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	// In Go, the := operator is a shortcut for declaring and initializing a variable in one line
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	//message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+//
+//	Go executes init functions automatically at program startup, after global variables have been initialized.
+//	For more about init functions, see Effective Go.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
