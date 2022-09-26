6047c04d92b92ea5bda8cb4515a08a2d52284795
Jiahao Si <jiahao.si@shopee.com>
2022-09-26 17:47:52 +0800

    return error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,14 +1,22 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	// In Go, the := operator is a shortcut for declaring and initializing a variable in one line
 
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
